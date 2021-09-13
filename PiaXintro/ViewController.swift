e588aad7089d3e4ee11349b110d7ae0485b6a1a5
Bill Mårtensson <bill@magictechnology.se>
2021-09-13 13:51:04 +0200

    Intro

--- PiaXintro/ViewController.swift
+++ PiaXintro/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fancyheader: UILabel!
+    
+    var mycounter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("nu har vi startat")
+        
+        fancyheader.text = "Banan"
+        
     }
 
-
+    
+    @IBAction func letsclick(_ sender: Any) {
+        
+        mycounter = mycounter + 1
+        
+        if(mycounter == 10)
+        {
+            mycounter = 0
+        }
+        
+        print(mycounter)
+        
+        fancyheader.text = String(mycounter)
+    }
+    
+    
 }
 
